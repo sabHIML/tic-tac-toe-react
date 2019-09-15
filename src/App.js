700a5f61317402a28fb2e663cfceda928dd8f53a
himel <himel.46@gmail.com>
2019-09-15 19:38:50 +0600

    redux init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import * as actionCreators from './state/actions'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class AppComponent extends Component {
+  render () {
+    return (
+      <div className="App">
+        i am testing!
+      </div>
+    )
+  }
+
+  componentDidMount () {
+  }
 }
+
+export const App = connect(
+  state => ({ }),
+  dispatch => bindActionCreators(actionCreators, dispatch)
+)(AppComponent);
 
 export default App;
