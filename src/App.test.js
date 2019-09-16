8aaf65c8a869b02ba8e9e98f038c237dd9c4af64
himel <himel.46@gmail.com>
2019-09-17 00:18:57 +0600

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,123 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import reducer from './state/reducers/gameStatus';
+import * as actions from './state/actions';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Tic Tac Toe', () => {
+  const emptyBoard = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ];
+
+  describe('Reducers', () => {
+
+    it('should start a new game', () => {
+      // the current state 
+      const state = {
+        board: emptyBoard,
+        gameover: true,
+        player: 1,
+        winner: -1,
+        moves:[]
+      };
+
+      const expectedState = {
+        board: emptyBoard.slice(),
+        gameover: false,
+        player: 1,
+        winner: -1,
+        moves:[]
+      };
+
+      const action = actions.newGame();
+      const result = reducer(state, action);
+
+      expect(result).toEqual(expectedState);
+    });
+
+    it('should end a game', () => {
+      const state = {
+        board: emptyBoard,
+        gameover: false,
+        player: 1,
+        winner: -1,
+        moves:[]
+      };
+
+      const expectedState = {
+        board: emptyBoard,
+        gameover: true,
+        player: 1,
+        winner: -1,
+        moves:[]
+      };
+
+      const action = actions.gameover();
+      const result = reducer(state, action);
+
+      expect(result).toEqual(expectedState);
+    });
+
+    it('should update the board and track moves when a player makes a move', () => {
+      const state = {
+        board: emptyBoard,
+        gameover: false,
+        player: 1,
+        winner: -1,
+        moves:[]
+      };
+
+      const player = 1;
+      const row = 2;
+      const col = 2;
+
+      const expectedState = {
+        board: [
+          [0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 1]
+        ],
+        gameover: false,
+        player: 1,
+        winner: -1,
+        moves:[{player:player,col:col,row:row}]
+      };
+
+ 
+
+      const action = actions.movePlayer(player, row, col);
+      const result = reducer(state, action);
+
+      expect(result).toEqual(expectedState);
+    });
+
+    it('should win a game', () => {
+      const winBoard = [
+        [2, 2, 0],
+        [1, 1, 1],
+        [2, 2, 0]
+      ];
+      const state = {
+        board: winBoard,
+        gameover: false,
+        player: 1,
+        winner: -1,
+        moves:[]
+      };
+
+      const expectedState = {
+        board: winBoard,
+        gameover: true,
+        player: 1,
+        winner: 1,
+        moves:[]
+      };
+
+      const action = actions.winner(1);
+      const result = reducer(state, action);
+
+      expect(result).toEqual(expectedState);
+    });
+
+    
+  });
 });
